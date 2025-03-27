44be0a166a7e0f976ce9a776c8eedf7ab7d64ad7
niquola <niquola@gmail.com>
2025-03-27 11:55:55 +0000

    progress

--- src/fhir/schema.clj
+++ src/fhir/schema.clj
@@ -14,7 +14,7 @@
 (declare *validate)
 
 (defn add-error [vctx error]
-  (update vctx :errors conj error))
+  (update vctx :errors conj (update error :path (fn [x] (or x (:path vctx))))))
 
 (defn resolve-type   [vctx type-ref]
   (get-in vctx [:ctx :schemas type-ref]))
@@ -24,14 +24,14 @@
 
 (defn add-schema-ref [vctx schema-ref schema-path]
   (if-let [sch (resolve-schema vctx schema-ref)]
-    (update vctx conj {:schema sch :path schema-path})
+    (update vctx :schemas conj {:schema sch :path (conj schema-path schema-ref)})
     (add-error vctx {:type :schema/unknown :schema schema-ref :schema-path schema-path})))
 
 (defn add-type-schema [vctx type-ref schema-path]
   (assert (string? type-ref) (pr-str type-ref))
   (println :add-type-schema type-ref)
   (if-let [sch (resolve-type vctx type-ref)]
-    (update vctx :schemas conj {:schema sch :path schema-path})
+    (update vctx :schemas conj {:schema sch :path (conj schema-path type-ref)})
     (add-error vctx {:type :type/unknown :schema type-ref :schema-path schema-path})))
 
 (def ARRAY_RULES [:min :max :slicing])
@@ -63,18 +63,58 @@
   (->> data
        (reduce (fn [data-els [k v]]
                  (if (str/starts-with? (name k) "_")
-                   ;; smart merge for ["ups" nil "ups"]
+                   ;;TODO: smart merge for ["ups" nil "ups"]
                    (let [el-k (keyword (subs (name k) 1))]
                      (update data-els el-k merge {:key el-k :path (conj (:path vctx) k) :extension v}))
                    (update data-els k merge {:key k :path (conj (:path vctx) k) :value v})))
                {})))
 
-(def RULES [:type])
+(defn validate-string [vctx schemas data]
+  (println "validate-string" (pr-str data) (string? data))
+  (if (not (string? data))
+    (add-error vctx {:type :type
+                     :message "Expected type string"
+                     :value data :schema-path (:path (first schemas))})
+    vctx))
 
-;; required, excluded, choice types
+(def TYPE_VALIDATORS {"string" #'validate-string})
+
+;; should we use kind?
+(defn validate-type [vctx schemas data]
+  (let [type-idx (group-by :schema schemas)]
+    (->> type-idx
+         (reduce (fn [vctx [type schemas]]
+                   (if-let [vld (get TYPE_VALIDATORS type)]
+                     (vld vctx schemas data)
+                     (add-error vctx {:type :type/unknown :value type :path (:path vctx)})))
+                 vctx))))
+
+(defn validate-kind [vctx schemas data]
+  (let [kinds-idx (group-by :schema schemas)]
+    (->> kinds-idx
+         (reduce (fn [vctx [kind schemas]]
+                   ;; (println :validate-kind kind schemas)
+                   vctx
+                   ) vctx))))
+
+(def VALUE_RULES {:type #'validate-type
+                  ;; :kind #'validate-kind
+                  })
+
 (defn validate-value-rules [vctx data]
-  (->> RULES
-       (reduce (fn [vctx rule-name] vctx) vctx)))
+  ;; (println :validate-value (:schemas vctx) data)
+  (->> VALUE_RULES
+       (reduce
+        (fn [vctx [rule-name rule-fn]]
+          (if-let [schemas (->> (:schemas vctx)
+                                (mapcat (fn [{sch :schema path :path}]
+                                          ;; (println :sch sch rule-name)
+                                          (when-let [rule (get sch rule-name)]
+                                            [{:schema rule :parent sch :path (conj path rule-name)}])))
+                                (seq))]
+            (rule-fn vctx schemas data)
+            vctx))
+        vctx)))
 
 (def ELEMENTS_RULES [:required :expected :choices :contstraint])
 
@@ -85,7 +125,7 @@
 (defn add-schemas [vctx data]
   (let [vctx (cond-> vctx
                (:resourceType data) (add-type-schema (:resourceType data) [])
-               (:type data)         (add-type-schema (:type data) []))
+               #_#_(:type data)         (add-type-schema (:type data) []))
         vctx (->> (get-in data [:meta :profile])
                   (reduce (fn [vctx profile] (add-schema-ref vctx profile [])) vctx))]
     (->> (:schemas vctx)
@@ -104,8 +144,8 @@
        (into #{})))
 
 (defn *validate [vctx data]
+  (println :*validate (:schemas vctx) data)
   (let [vctx  (add-schemas vctx data)
-        ;; _ (println :path (:path vctx) "\n" :schemas "\n  " (str/join "\n   " (:schemas vctx)) "\n" :data data)
         vctx (validate-value-rules vctx data)]
     (if (map? data)
       (let [elements (data-elements vctx data)
@@ -122,24 +162,19 @@
 
 (defn mk-validation-context [ctx schema-refs resource]
   (let [vctx {:ctx ctx :errors [] :deferreds [] :resource resource :path [] :schemas #{}}]
-    (reduce (fn [vctx schema-ref] (add-schema-ref vctx schema-ref [])) vctx schema-refs )))
+    (reduce (fn [vctx schema-ref] (add-schema-ref vctx schema-ref [schema-ref])) vctx schema-refs )))
+
+(defn validate-schemas [ctx schemas resource]
+  (let [vctx (mk-validation-context ctx [] resource)
+        vctx (->> schemas
+             (reduce (fn [vctx sch] (update vctx :schemas conj {:schema sch :path []}))
+                     vctx))]
+    (select-keys (*validate vctx resource) [:errors :deferreds])))
 
 (defn validate [ctx schema-refs resource]
   (let [vctx (mk-validation-context ctx schema-refs resource)]
-    (*validate vctx resource)))
+    (select-keys (*validate vctx resource) [:errors :deferreds])))
 
 
-(def ctx {:schemas {"HumanName" {:elements {:family {:type "string"}
-                                            :given  {:array true :type "string"}
-                                            :use    {:type "Coding"}}}
-                    "Patient" {:base "Resource"
-                               :elements {:name {:array true :type "HumanName"}}}
-                    "Coding" {:elements {:code {:type "string"}}}
-                    "Resource" {:elements {:resourceType {:type "code"}}}
-                    "code" {:kind "primitive-type"}
-                    "string" {:kind "primitive-type"}}})
 
-(:errors (validate ctx [] {:resourceType "Patient" :name [{:family "f" :given ["g1" "g2"]}]}))
-(:errors (validate ctx [] {:resourceType "Patient" :name [{:family "f"
-                                                           :use {:code "x" :ups "x"}
-                                                           :given ["g1" "g2"]}]}))
+
