679811e5fc3e1f027f12cb82b65ddc86ef58f7be
Aleksandr Penskoi <alexandr.penskoi@health-samurai.io>
2025-04-18 11:19:16 +0200

    [misc] Use HealthSamurai/golden-clj instead local implementation.

--- test/transpiler/golden_test.clj
+++ test/transpiler/golden_test.clj
@@ -1,53 +1,46 @@
 (ns transpiler.golden-test
-  (:require [clojure.test :refer :all]
-            [fhir.schema.translate :as fhir-schema]
-            [cheshire.core :as json]
-            [clojure.string :as str]
-            [clojure.java.io :as io]
-            [matcho.core :as matcho]))
+  (:require
+   [cheshire.core :as json]
+   [clojure.java.io :as io]
+   [clojure.test :refer [deftest is run-tests]]
+   [fhir.schema.translate :as fhir-schema]
+   [golden.core :as golden]))
 
-(defn update-golden? []
-  (= "true" (System/getenv "UPDATE_GOLDEN")))
-
-(defn golden-file-content [expect-filename actual-content]
-  (let [expect-content (when (.exists (io/file expect-filename))
-                         (slurp expect-filename))]
-    (cond
-      (or (nil? expect-content)
-          (update-golden?))
-      (do (spit expect-filename actual-content)
-          [actual-content actual-content])
-
-      :else
-      [expect-content actual-content])))
-
-(defn fhir-schema-content [sd-filename]
-  (let [fs-filename (str/replace sd-filename #".sd.json$" ".fs.json")
-        sd          (-> (slurp sd-filename)
-                        (json/parse-string true))
-        fs-json     (-> sd
-                        (fhir-schema/translate)
-                        (json/generate-string {:pretty true}))]
-    (golden-file-content fs-filename fs-json)))
-
-(defmacro golden-test [get-content filename & [as-text]]
-  (let [f (if as-text identity #(json/parse-string % true))]
-    `(let [[expect-content# actual-content#] (->> (~get-content ~filename)
-                                                  (map ~f))]
-       (is (= expect-content# actual-content#)
-           (str "golden-fhir-schema check fail for: " ~filename)))))
+(defn sd->fhir-schema [sd-filename]
+  (-> (slurp sd-filename)
+      (json/parse-string true)
+      (fhir-schema/translate)))
 
 (deftest structure-definition-test
-  (golden-test fhir-schema-content "test/golden/patient.sd.json")
-  (golden-test fhir-schema-content "test/golden/questionnaire.sd.json")
-  (golden-test fhir-schema-content "test/golden/bundle.sd.json")
-  (golden-test fhir-schema-content "test/golden/primitive/string.sd.json")
-  (golden-test fhir-schema-content "test/golden/primitive/unsignedInt.sd.json")
-  (golden-test fhir-schema-content "test/golden/primitive/boolean.sd.json")
-  (golden-test fhir-schema-content "test/golden/complex/element.sd.json")
-  (golden-test fhir-schema-content "test/golden/complex/address.sd.json")
-  (golden-test fhir-schema-content "test/golden/complex/extension.sd.json")
-  (golden-test fhir-schema-content "test/golden/complex/backbone-element.sd.json"))
+  (golden/as-json "test/golden/patient.fs.json"
+                  (sd->fhir-schema "test/golden/patient.sd.json"))
+
+  (golden/as-json "test/golden/questionnaire.fs.json"
+                  (sd->fhir-schema "test/golden/questionnaire.sd.json"))
+
+  (golden/as-json "test/golden/bundle.fs.json"
+                  (sd->fhir-schema "test/golden/bundle.sd.json"))
+
+  (golden/as-json "test/golden/primitive/string.fs.json"
+                  (sd->fhir-schema "test/golden/primitive/string.sd.json"))
+
+  (golden/as-json "test/golden/primitive/unsignedInt.fs.json"
+                  (sd->fhir-schema "test/golden/primitive/unsignedInt.sd.json"))
+
+  (golden/as-json "test/golden/primitive/boolean.fs.json"
+                  (sd->fhir-schema "test/golden/primitive/boolean.sd.json"))
+
+  (golden/as-json "test/golden/complex/element.fs.json"
+                  (sd->fhir-schema "test/golden/complex/element.sd.json"))
+
+  (golden/as-json "test/golden/complex/address.fs.json"
+                  (sd->fhir-schema "test/golden/complex/address.sd.json"))
+
+  (golden/as-json "test/golden/complex/extension.fs.json"
+                  (sd->fhir-schema "test/golden/complex/extension.sd.json"))
+
+  (golden/as-json "test/golden/complex/backbone-element.fs.json"
+                  (sd->fhir-schema "test/golden/complex/backbone-element.sd.json")))
 
 (comment
   (run-tests 'transpiler.core-test)
